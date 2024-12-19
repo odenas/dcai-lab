f341a48635e81d352ce44d7d954d925ba645dfce
Olgert Denas <gertidenas@gmail.com>
2024-12-19 10:45:20 -0800

    remove digression

--- data_centric_model_centric/digression_code/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-from .tasks import Task
-from .pt_model import PTModel
-from .ft_model import FTModel, FTFExtractor, Answer
-from .utils import show_examples, show_predictions
-from .aletheia_data import ImageFolderDataset, ads_id, load_annotations_csv
-from .confident_learning import CLIssues
